4a2b9521e9d0edc51942d423d1b350fcaf74f6c9
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-09-15 16:45:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Entry point: starts the Express server
 const app = require('./src/app');
 
-const port = process.env.PORT || 8080;
-const host = '0.0.0.0';
+const port = process.env.PORT || 443;
+const host = 'x-468277386481.us-east1.run.app';
 
-app.listen(port, host, () => {
+app.listen(port,host, () => {
   console.log(`🚀 Looker Microservice running on http://${host}:${port}`);
 });
