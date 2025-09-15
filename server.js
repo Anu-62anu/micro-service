b12a2a549130d0150cdfa63394596c56f67c44e2
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-09-15 18:25:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Entry point: starts the Express server
 const app = require('./src/app');
 
-const port = process.env.PORT || 443;
-const host = 'x-468277386481.us-east1.run.app';
+const port = process.env.PORT || 8080;
+// const host = 'x-468277386481.us-east1.run.app';
 
-app.listen(port,host, () => {
-  console.log(`Looker Microservice running on http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`Looker Microservice running on port ${port}`);
 });
