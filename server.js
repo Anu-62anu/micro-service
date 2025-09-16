aa40c1cd3e3718cc51084c4d9f8c353bda3e99cc
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-09-16 12:03:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = require('./src/app');
 
 const port = process.env.PORT || 443;
 // const host = 'x-468277386481.us-east1.run.app';
+// const host = '0.0.0.0';
 
 app.listen(port, () => {
+  // console.log(`Looker Microservice running on http://${host}:${port}`);
   console.log(`Looker Microservice running on port ${port}`);
 });
