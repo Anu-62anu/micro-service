eb43c3d3a2cec9421cbdb22ed2b58f9b45613bd8
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-09-16 10:54:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Entry point: starts the Express server
 const app = require('./src/app');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 // const host = 'x-468277386481.us-east1.run.app';
 
 app.listen(port, () => {
